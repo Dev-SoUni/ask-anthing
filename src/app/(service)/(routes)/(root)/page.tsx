b9198758b08a939f166d5f8e28b01bb7406ba882
Byun SoYun <bsy1482@naver.com>
2024-04-18 22:32:02 +0900

    feat: 프로젝트 기본 설정
    - shadcn
    - globals.css 초기 설정
    - metadata 설정

---
+++ src/app/(service)/(routes)/(root)/page.tsx
@@ -0,0 +1,11 @@
+import type { Metadata } from "next"
+
+export const metadata: Metadata = {
+  title: "HOME",
+}
+
+export default function Home() {
+  return (
+    <></>
+  );
+}
---
+++ src/app/(service)/layout.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+export default function ServiceLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <div>
+      {children}
+    </div>
+  )
+}
